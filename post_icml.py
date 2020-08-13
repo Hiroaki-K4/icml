30141720de847e981bcf7c4ad8c25016497fb46a
Hiroaki-k4 <kubo@globalwalkers.co.jp>
2020-08-13 11:30:52 +0900

    edit

--- post_icml.py
+++ post_icml.py
@@ -1,12 +1,26 @@
-
+import os
+import csv
+import tempfile
+from google.cloud import storage
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='/home/hiroaki-k4/Downloads/first-penguin-284413-5b7ff1cd5e53.json'
 
 
 
 
 
 def main():
-    
-
+    with tempfile.TemporaryDirectory() as temp_path:
+        write_path = temp_path + '/icml_edit.csv'
+        client = storage.Client()
+        bucket_name = "penguin-first"
+        bucket = client.get_bucket(bucket_name)
+        blob = bucket.get_blob('icml_edit.csv')
+        blob.download_to_filename(write_path)
+        with open(write_path) as f:
+            reader = csv.reader(f)
+            word_list = [row for row in reader]
+        print(word_list)
+        input()
 
 
 
